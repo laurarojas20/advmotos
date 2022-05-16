7466c67d11dc7a3558f1876747711f7ba3e831a3
laurarojas20 <laurarojasss@hotmail.com>
2022-05-16 15:39:47 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar/NavBar'; 
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="NavBar">
+      
+        <NavBar/>
+      
     </div>
+    <div> 
+      <h2 className='App'>
+        Próximamente tienda de accesorios de motos ADV 
+      </h2>
+
+    </div>
+    </>
   );
 }
 
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import { Navbar } from "react-bootstrap" 
+import { Nav } from "react-bootstrap"
+import { NavDropdown } from "react-bootstrap"
+import { Container } from "react-bootstrap"
+
+const NavBar = () => {
+    
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home"> ATVMotos </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features"> Pestaña 1 </Nav.Link>
+      <Nav.Link href="#pricing"> Pestaña 2 </Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1"> Pestaña Drop1 </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2"> Pestaña Drop2 </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3"> PEstaña Drop3 </NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4"> Pestaña separada Drop4 </NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets"> Pestaña derecha </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
+
+export default NavBar
